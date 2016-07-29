858d3b7ad82a45d62df41ebc97f10789a87214e0
jooa86 <jooa86@gmail.com>
2016-07-29 12:23:06 +0300

    tehdään esimerkistä toiminnallisempi

--- pi_test.py
+++ pi_test.py
@@ -1,5 +1,20 @@
 import RPi.GPIO as GPIO
+import time
 
+# Settings
+pwrLedPin = 18
+statusLedPin = 23
+buttonPin = 17
+dutyCycle = 95
+
+# Pin setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-GPIO.output(18, True)
+GPIO.setup(pwrLedPin, GPIO.OUT)
+GPIO.setup(statusLedPin, GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+pwr = GPIO.PWM(pwrLedPin, 50)
+
+# Initial state for LEDs
+GPIO.output(statusLedPin, GPIO.low)
+pwm.start(dutyCycle);
+print("Up and running! Press CTRL+C to exit")
