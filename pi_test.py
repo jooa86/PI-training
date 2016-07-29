0dca318792029ead09e75a93c5dca102da65e9d9
jooa86 <jooa86@gmail.com>
2016-07-29 12:27:51 +0300

    toiminnallisuus loppuun

--- pi_test.py
+++ pi_test.py
@@ -12,9 +12,24 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(pwrLedPin, GPIO.OUT)
 GPIO.setup(statusLedPin, GPIO.OUT)
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-pwr = GPIO.PWM(pwrLedPin, 50)
+pwm = GPIO.PWM(pwrLedPin, 50)
 
 # Initial state for LEDs
-GPIO.output(statusLedPin, GPIO.low)
+GPIO.output(statusLedPin, GPIO.LOW)
 pwm.start(dutyCycle);
 print("Up and running! Press CTRL+C to exit")
+
+try:
+    while 1:
+        if GPIO.input(buttonPin): # button released
+            pwm.ChangeDutyCycle(dutyCycle)
+            GPIO.output(statusLedPin, GPIO.LOW)
+        else: # button pressed
+            pwm.ChangeDutyCycle(100 - dutyCycle)
+            GPIO.output(statusLedPin, GPIO.HIGH)
+            time.sleep(0.075)
+            GPIO.output(statusLedPin, GPIO.LOW)
+            time.sleep(0.075)
+except KeyboardInterrupt: #If CTRL+C is pressed
+    pwm.stop()
+    GPIO.cleanup()
